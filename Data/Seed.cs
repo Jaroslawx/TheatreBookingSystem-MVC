34d8c4a135c145a2317ae9ce8bbde9950b88475c
JaroslawX <ad.jaro@wp.pl>
2023-12-05 12:13:50 +0100

    Update seed.cs

--- Data/Seed.cs
+++ Data/Seed.cs
@@ -1,10 +1,39 @@
-﻿namespace TheatreBookingSystem_MVC.Data
+﻿using System.Net;
+using TheatreBookingSystem_MVC.Models;
+
+namespace TheatreBookingSystem_MVC.Data
 {
     public class Seed
     {
         public static void SeedData(IApplicationBuilder applicationBuilder)
         {
-            // Here we can add seed data to the database
+            // Here add seed data to the database
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                context.Database.EnsureCreated();
+
+                if (!context.Buildings.Any())
+                {
+                    context.Buildings.AddRange(new List<Building>()
+                    {
+                        new Building()
+                        {
+                            Name = "Building 1",
+                            Street = "Wiejska 5",
+                            City = "Bialystok"
+                         },
+                        new Building()
+                        {
+                            Name = "Building 2",
+                            Street = "Wiejska 6",
+                            City = "Bialystok"
+                        }
+                    });
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
