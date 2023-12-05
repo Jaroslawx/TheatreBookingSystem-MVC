80a2ceca66838cdbcb9aa57166d38b554b4448ff
JaroslawX <ad.jaro@wp.pl>
2023-12-05 12:36:06 +0100

    updates, add controllers

---
+++ Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheatreBookingSystem_MVC.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
