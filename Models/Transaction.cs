558f5dc5e6c82a10068cdab5d7f90ff3e4898026
JaroslawX <ad.jaro@wp.pl>
2023-12-18 10:50:31 +0100

    Update transaction and dbcontext

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -7,19 +7,14 @@ namespace TheatreBookingSystem_MVC.Models
     {
         [Key]
         public int? Id { get; set; }
-
-        [Required]
         public decimal Price { get; set; }
-
-        [Required]
-        public string BuyerName { get; set; }
-
-        [Required]
         public DateTime PurchaseDate { get; set; }
-
         [ForeignKey("Ticket")]
         public int? TicketId { get; set; }
         public Ticket? Ticket { get; set; }
+        [ForeignKey("AppUser")]
+        public int? AppUserId { get; set; }
+        public AppUser? AppUser { get; set; }
     }
 }
 
