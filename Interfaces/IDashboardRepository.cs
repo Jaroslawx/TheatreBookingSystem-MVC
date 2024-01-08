68e7daf8ea9eb2efaa45a87fe215877606ebe6ce
JaroslawX <ad.jaro@wp.pl>
2024-01-08 13:48:02 +0100

    Dashboard create, update what shows for user roles and when logged in/out

---
+++ Interfaces/IDashboardRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheatreBookingSystem_MVC.Interfaces
+{
+    public class IDashboardRepository : Controller
+    {
+        Task<List<IEventRepository>> GetAllUserEvents;
+        Task<List<IEventRepository>> GetAllEvents;
+    }
+}
---
+++ Repository/DashboardRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheatreBookingSystem_MVC.Data;
+using TheatreBookingSystem_MVC.Interfaces;
+
+namespace TheatreBookingSystem_MVC.Repository
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _HttpContextAccessor;
+        public DashboardRepository(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _HttpContextAccessor = httpContextAccessor;
+        }
+        public async Task<List<IEventRepository>> GetAllReservations()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
