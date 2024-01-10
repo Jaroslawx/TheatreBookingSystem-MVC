4eef4b85f824cf549585f1fed86ffffd36f80818
wikto <wikto@WIKTORIA>
2024-01-10 23:47:35 +0100

    created sign up form page and css

---
+++ Views/Login/Signin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheatreBookingSystem_MVC.Views.Login
+{
+    public class SigninModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
