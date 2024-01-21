dcdab9e34766c72e91c5944dc2a42fa3b0147bff
Cezary Daniluk <82672@student.pb.edu.pl>
2024-01-21 23:46:52 +0100

    Integrated dashboard with transactions

---
+++ Models/ChatMessage.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheatreBookingSystem_MVC.Models
+{
+    public class ChatMessage
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string AppUserId { get; set; }
+    }
+}
