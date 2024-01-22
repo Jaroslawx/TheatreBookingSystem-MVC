e4e6f1405ba8070027650e11b4eeeca00c6c7277
wikto <wikto@WIKTORIA>
2024-01-22 03:21:21 +0100

    profile page and settings page + css

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -8,5 +8,10 @@ namespace TheatreBookingSystem_MVC.Controllers
         {
             return View();
         }
+
+        public IActionResult ProfileSettings()
+        {
+            return View();
+        }
     }
 }
